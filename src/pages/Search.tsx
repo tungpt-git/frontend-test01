df9a8d3987f6404dcdffd219cb5c06ae77388459
thuyanh.vuong <thuyanh.vuong>
2021-06-13 16:43:39 +0700

    update home

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Typography, Box, makeStyles } from "@material-ui/core";
 import { useHistory } from "react-router";
 import SearchBar from "../components/SearchBar/SearchBar";
 import { ROUTES } from "../routers";
+import clsx from "clsx";
+import shadows from "@material-ui/core/styles/shadows";
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    height: 500,
+    width: (500 * 16) / 9,
+    background: theme.palette.primary.main,
+    borderRadius: "24px",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  centerContent: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
 
 export default function Search() {
+  const classes = useStyles();
   const history = useHistory();
 
   return (
     <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-      minWidth="fit-content"
+      className={classes.centerContent}
+      style={{ height: "100vh", minWidth: "fit-content" }}
     >
-      <SearchBar
-        onSubmit={(query: string) => {
-          if (!query) return;
-          history.push(
-            ROUTES.SEARCH_RESULT + `?query=${encodeURIComponent(query)}`
-          );
-        }}
-      />
+      <Box
+        boxShadow={shadows[3]}
+        className={clsx(classes.wrapper, classes.centerContent)}
+      >
+        <Typography
+          variant="h2"
+          style={{ marginBottom: 24, color: "white", fontWeight: 700 }}
+        >
+          Welcome
+        </Typography>
+        <SearchBar
+          onSubmit={(query: string) => {
+            if (!query) return;
+            history.push(
+              ROUTES.SEARCH_RESULT + `?query=${encodeURIComponent(query)}`
+            );
+          }}
+        />
+      </Box>
     </Box>
   );
 }
